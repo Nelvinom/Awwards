ae7040c9f2a404c017623b9b50066381da577d91
Nelvinom <omondinelvin884@gmail.com>
2020-06-11 00:25:04 +0300

    commit

--- awwards/settings.py
+++ awwards/settings.py
@@ -81,12 +81,16 @@ WSGI_APPLICATION = 'awwards.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #      'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'awward',
+    #     'USER': 'moringa',
+    # 'PASSWORD':'@young',
+    # 'port':'5432'
+    # }
     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'awward',
-        'USER': 'moringa',
-    'PASSWORD':'@young',
-    'port':'5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
